856d1962d6c07f0ad1841870f553672c33982d2b
k-okina <okinakahiro@gmail.com>
2018-01-24 15:26:15 +0900

    詳細画面追加してみる

--- webook/views.py
+++ webook/views.py
@@ -5,3 +5,8 @@ from webook.models import Post
 def post_list(request):
     posts = Post.published_list()
     return render(request, 'webook/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request, 'webook/post_detail.html', {'post': post})
