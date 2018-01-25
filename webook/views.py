40404f4e07680ff9bdc448933d1c05dcaecfee6b
k-okina <okinakahiro@gmail.com>
2018-01-25 09:45:17 +0900

    tutorialとりあえず完了

--- webook/views.py
+++ webook/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from webook.models import Post
+from webook.forms import PostForm
+from django.shortcuts import redirect
 
 
 def post_list(request):
-    posts = Post.published_list()
+    posts = Post.objects.all()
     return render(request, 'webook/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = Post.objects.get(id=pk)
     return render(request, 'webook/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.id)
+    else:
+        form = PostForm()
+    return render(request, 'webook/post_new.html', {'form': form})
