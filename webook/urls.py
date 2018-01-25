b4784e0e455dc5f641b5d397e8ddeccd49818c14
k-okina <okinakahiro@gmail.com>
2018-01-25 11:47:25 +0900

    ホーム画面デザイン、レスポンシブ対応作成

--- webook/urls.py
+++ webook/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from webook import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)$', views.post_detail, name='post_detail'),
     url(r'^post/new$', views.post_new, name='post_new'),
--- webook/views.py
+++ webook/views.py
@@ -4,14 +4,18 @@ from webook.forms import PostForm
 from django.shortcuts import redirect
 
 
+def home(request):
+    return render(request, 'webook/home/index.html')
+
+
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'webook/post_list.html', {'posts': posts})
+    return render(request, 'webook/books/index.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = Post.objects.get(id=pk)
-    return render(request, 'webook/post_detail.html', {'post': post})
+    return render(request, 'webook/books/detail.html', {'post': post})
 
 
 def post_new(request):
@@ -25,4 +29,4 @@ def post_new(request):
             return redirect('post_detail', pk=post.id)
     else:
         form = PostForm()
-    return render(request, 'webook/post_new.html', {'form': form})
+    return render(request, 'webook/books/create.html', {'form': form})
