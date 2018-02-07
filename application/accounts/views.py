46846dd606a80c47edcadef9a5664bcb0d63cf34
k-okina <okinakahiro@gmail.com>
2018-02-07 11:48:29 +0900

    ユーザー登録 => 自動ログイン welcomeメッセージの設定完了

--- application/accounts/views.py
+++ application/accounts/views.py
@@ -1,23 +1,37 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import login
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib import messages
+from application.helper import Log
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('accounts:login')
+    success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
     def form_valid(self, form):
         """バリデーションを通った時"""
+        # ユーザー登録をする
         form = UserCreationForm(self.request.POST)
 
-        if form.is_valid():
-            new_user = form.save()
-            new_user = authenticate(username=form.cleaned_data['username'],
-                                    password=form.cleaned_data['password1'])
-            login(self.request, new_user)
-        return HttpResponseRedirect('/')
+        # get_success_urlでself.objectにformが入っている前提でコードが書かれている
+        self.object = form.save()
+        messages.success(self.request, "Welcome to Webook app!")
+        messages.success(self.request, "It succeeded in user registration!")
+        messages.success(self.request, "Let's immediately read the book together now!")
+        Log.info('success to signup.')
+
+        # 自動ログインする
+        user = authenticate(self.request,
+                            username=self.request.POST['username'],
+                            password=self.request.POST['password1'])
+
+        if user is not None:
+            login(self.request, user)
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            Log.info('i don\'t no why, failed login.')
+            return HttpResponseRedirect(reverse_lazy('login'))
--- application/webook/views.py
+++ application/webook/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
+from application.helper import Log
 
 
 def home(request):
+    Log.info(request.user.is_authenticated)
     return render(request, 'webook/home/index.html')
