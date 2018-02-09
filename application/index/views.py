d01a12066f481d7ded7eabd9bf5d06000044fea4
k-okina <okinakahiro@gmail.com>
2018-02-09 14:39:29 +0900

    dashboardは消えたので修正

--- application/index/views.py
+++ application/index/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 
 
-def dashboard(request):
+@require_GET
+def index(request):
     return render(request, 'index/index.html')
