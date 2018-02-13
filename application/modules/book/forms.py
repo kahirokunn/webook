ede522b63cbe1503a6ce135667a9a3d06a566f7d
k-okina <okinakahiro@gmail.com>
2018-02-13 13:48:08 +0900

    formの作成と、templateの呼び出し順番修正

--- application/modules/book/forms.py
+++ application/modules/book/forms.py
@@ -1,15 +1,12 @@
-from django import forms
-from modules.book.constants import BOOK_TYPES
+from django.forms import URLField, ModelForm
+from .models import Book
 
 
-class Form(forms.Form):
-    char_field = forms.CharField()
-    choice_field = forms.ChoiceField(choices=BOOK_TYPES)
-    radio_choice = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
-    multiple_choice = forms.MultipleChoiceField(choices=CHOICES)
-    multiple_checkbox = forms.MultipleChoiceField(choices=CHOICES,
-                                                  widget=forms.CheckboxSelectMultiple)
-    file_fied = forms.FileField()
-    password_field = forms.CharField(widget=forms.PasswordInput)
-    textarea = forms.CharField(widget=forms.Textarea)
-    boolean_field = forms.BooleanField()
+class NewForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ('title',
+                  'thumbnail_url',
+                  'orderd_page_url',
+                  'book_url',
+                  'type')
--- application/modules/order_book/forms.py
+++ application/modules/order_book/forms.py
@@ -1,14 +1,38 @@
-from django.forms import CharField, ModelForm
-from .models.book import Book
+from django.forms import DateField, IntegerField, ModelForm, Media
+from .models import OrderBook
+from ..book.forms import NewForm as BookForm
+from django.contrib.admin.widgets import AdminDateWidget
+from django.contrib.admin.options import ModelAdmin
+from settings import DEBUG
 
 
-class NewForm(ModelForm):
-    class Meta:
-        model = Book
-        fields = ('title',
-                  'thumbnail_url',
-                  'orderd_page_url',
-                  'book_url',
-                  'type')
+def admin_datepicker_media():
+    extra = '' if DEBUG else '.min'
+    css = ('forms.css',)
+    js = (
+        'vendor/jquery/jquery%s.js' % extra,
+        'jquery.init.js',
+        'core.js',
+        'admin/RelatedObjectLookups.js',
+        'actions%s.js' % extra,
+        'urlify.js',
+        'prepopulate%s.js' % extra,
+        'vendor/xregexp/xregexp%s.js' % extra,
+        'calendar.js',
+        'admin/DateTimeShortcuts.js',
+    )
+    return Media(js=['admin/js/%s' % url for url in js],
+                 css={'all': ['admin/css/%s' % url for url in css]})
 
-    thumbnail_url = orderd_page_url = book_url = CharField()
+
+class NewForm(BookForm):
+    price = IntegerField()
+    ordered_at = DateField(widget=AdminDateWidget)
+
+    class Meta(BookForm.Meta):
+        fields = BookForm.Meta.fields + ('price', 'ordered_at')
+
+    @property
+    def media(self):
+        # ここに書いたcss, jsがstaticで呼び出される
+        return admin_datepicker_media
