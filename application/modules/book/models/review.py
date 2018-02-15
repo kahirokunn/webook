9dfe3c7c415ddd1bf85b0ca1dcef8230d2b63fa6
k-okina <okinakahiro@gmail.com>
2018-02-15 14:15:43 +0900

    book > reviewの追加

---
+++ application/modules/book/models/review.py
@@ -0,0 +1,32 @@
+from django.db import models
+from submodules.custom_model_fields import IntegerRangeField
+import settings
+
+
+class Review(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=False)
+    book = models.ForeignKey(to='Book', on_delete=False)
+    text = models.TextField()
+    star = IntegerRangeField(min_value=1, max_value=5)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def get_all(cls) -> list:
+        return list(cls.objects.all().order_by('-created_at'))
+
+    @classmethod
+    def get_by_user(cls, user) -> list:
+        return list(cls.objects.filter(user=user).order_by('-created_at'))
+
+    @classmethod
+    def get_by_id(cls, pk: int):
+        return cls.objects.get(pk=pk)
+
+    @classmethod
+    def is_exists_by_user(cls, user) -> bool:
+        return cls.objects.get(user=user).exists()
+
+    def __str__(self):
+        return self.name
